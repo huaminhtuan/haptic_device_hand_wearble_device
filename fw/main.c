ee9f16c8d17fc39ecd0a89a6ff9afb5fa5c9e783
Hua Minh Tuan <huaminhtuan94@gmail.com>
2022-05-25 16:27:20 +0200

    add uart configuration function, need to code event handler function

--- fw/main.c
+++ fw/main.c
@@ -1,16 +1,23 @@
 #include <nrf52840.h>
+#include "sdk_config.h"
 #include "nrf.h"
 #include "nrf_esb.h"
 #include "nrf_error.h"
 #include "sdk_macros.h"
 #include "app_error.h"
+#include "nrfx_uart.h"
 
 #define CENTRAL_CONTROLLER
 //#define HAPTIC_DEVICE
 
+#define TX_PIN 25
+#define RX_PIN 24
+
 static nrf_esb_payload_t tx_payload = NRF_ESB_CREATE_PAYLOAD(0, 0x01, 0x00);
 static nrf_esb_payload_t rx_payload;
 static volatile bool esb_completed = false;
+
+static nrfx_uart_t uart_inst = NRFX_UART_INSTANCE(0);
 
 void nrf_esb_event_handler(nrf_esb_evt_t const * p_event)
 {
@@ -77,6 +84,51 @@ void clocks_start( void )
     while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
 }
 
+void uart_event_handler(nrfx_uart_event_t const * p_event, void *p_context)
+{
+	if (p_event->type == NRFX_UART_EVT_RX_DONE)
+	{
+//        // Received bytes counter has to be checked, because there could be event from RXTO interrupt
+//        if (p_event->data.rxtx.bytes)
+//        {
+//            app_uart_evt_t app_uart_event;
+//            app_uart_event.evt_type   = APP_UART_DATA;
+//            app_uart_event.data.value = p_event->data.rxtx.p_data[0];
+//            rx_done = true;
+//            m_event_handler(&app_uart_event);
+//        }
+//        (void)nrf_drv_uart_rx(&app_uart_inst, rx_buffer, 1);
+	}
+	else if (p_event->type == NRFX_UART_EVT_ERROR)
+	{
+//        app_uart_evt_t app_uart_event;
+//        app_uart_event.evt_type                 = APP_UART_COMMUNICATION_ERROR;
+//        app_uart_event.data.error_communication = p_event->data.error.error_mask;
+//        (void)nrf_drv_uart_rx(&app_uart_inst, rx_buffer, 1);
+//        m_event_handler(&app_uart_event);
+	}
+	else if (p_event->type == NRFX_UART_EVT_TX_DONE)
+	{
+//       // Last byte from FIFO transmitted, notify the application.
+//       // Notify that new data is available if this was first byte put in the buffer.
+//       app_uart_evt_t app_uart_event;
+//       app_uart_event.evt_type = APP_UART_TX_EMPTY;
+//       m_event_handler(&app_uart_event);
+	}
+}
+
+void uart_init()
+{
+	nrfx_uart_config_t config = NRFX_UART_DEFAULT_CONFIG;
+    config.pseltxd = TX_PIN;
+    config.pselrxd = RX_PIN;
+    config.hwfc = NRF_UART_HWFC_DISABLED;
+    config.parity = NRF_UART_PARITY_INCLUDED;
+	config.baudrate = NRF_UART_BAUDRATE_115200;
+	config.interrupt_priority = 7;
+	nrfx_uart_init(&uart_inst, &config, &uart_event_handler);
+}
+
 int main(void)
 {
 	uint32_t err_code;
@@ -84,5 +136,8 @@ int main(void)
 	clocks_start();
 	err_code = esb_init();
 	APP_ERROR_CHECK(err_code);
+
+	uart_init();
+
 	return 0;
 }
